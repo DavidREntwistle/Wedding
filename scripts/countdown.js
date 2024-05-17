50aea5d86977131fe2df61ee26921e6c5931b9cd
DavidREntwistle <davidrentwistle@gmail.com>
2024-05-17 01:25:02 +0100

    Splitting the countdown timer into header and nav functions

--- scripts/countdown.js
+++ scripts/countdown.js
@@ -1,8 +1,9 @@
 // countdown.js
-// Countdown timer
-let timer = setInterval(updateCountdown, 1000);
+// Countdown timer for the header section
+let headerTimer = setInterval(updateHeaderCountdown, 1000);
+let navTimer = setInterval(updateNavCountdown, 1000);
 
-function updateCountdown() {
+function updateHeaderCountdown() {
     // Get the current date and time
     const now = new Date();
 
@@ -19,16 +20,16 @@ function updateCountdown() {
     const seconds = Math.floor((diff % (1000 * 60)) / 1000);
 
     // Get the countdown timer element
-    const timerElement = document.getElementById("timer");
+    const timerHeaderElement = document.getElementById("header-timer");
 
     // Check the conditions to display appropriate message
     if (now >= weddingDate) {
         // Display "Just Married!" message
-        clearInterval(timer); // Stop the countdown
-        timerElement.innerHTML = `<div class="message">Just Married!</div>`;
+        clearInterval(headerTimer); // Stop the countdown
+        timerHeaderElement.innerHTML = `<div class="message">Just Married!</div>`;
     } else if (days === 0 && hours < 24) {
         // Display "Today is the day!" message above the countdown
-        timerElement.innerHTML = `<div class="message">Today is the day!</div>
+        timerHeaderElement.innerHTML = `<div class="message">Today is the day!</div>
         <div class="days">
             <div class="numbers">${days}</div>days
         </div>
@@ -43,7 +44,64 @@ function updateCountdown() {
         </div>`;
     } else if (days > 0 || (days === 0 && hours >= 24)) {
         // Display countdown
-        timerElement.innerHTML =
+        timerHeaderElement.innerHTML =
+            `<div class="days">
+            <div class="numbers">${days}</div>days
+        </div>
+        <div class="hours">
+            <div class="numbers">${hours}</div>hours
+        </div>
+        <div class="minutes"> 
+            <div class="numbers">${minutes}</div>minutes
+        </div>
+        <div class="seconds">
+            <div class="numbers">${seconds}</div>seconds
+        </div>`;
+    }
+}
+
+function updateNavCountdown() {
+    // Get the current date and time
+    const now = new Date();
+
+    // Set the wedding date and time in local timezone (Ireland)
+    const weddingDate = new Date('March 7, 2025 13:00:00 GMT+0000');
+
+    // Calculate the difference in milliseconds
+    const diff = weddingDate - now;
+
+    // Calculate days, hours, minutes, and seconds
+    const days = Math.floor(diff / (1000 * 60 * 60 * 24));
+    const hours = Math.floor((diff % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+    const minutes = Math.floor((diff % (1000 * 60 * 60)) / (1000 * 60));
+    const seconds = Math.floor((diff % (1000 * 60)) / 1000);
+
+    // Get the countdown timer element
+    const timerNavElement = document.getElementById("nav-timer");
+
+    // Check the conditions to display appropriate message
+    if (now >= weddingDate) {
+        // Display "Just Married!" message
+        clearInterval(navTimer); // Stop the countdown
+        timerNavElement.innerHTML = `<div class="message">Just Married!</div>`;
+    } else if (days === 0 && hours < 24) {
+        // Display "Today is the day!" message above the countdown
+        timerNavElement.innerHTML = `<div class="message">Today is the day!</div>
+        <div class="days">
+            <div class="numbers">${days}</div>days
+        </div>
+        <div class="hours">
+            <div class="numbers">${hours}</div>hours
+        </div>
+        <div class="minutes"> 
+            <div class="numbers">${minutes}</div>minutes
+        </div>
+        <div class="seconds">
+            <div class="numbers">${seconds}</div>seconds
+        </div>`;
+    } else if (days > 0 || (days === 0 && hours >= 24)) {
+        // Display countdown
+        timerNavElement.innerHTML =
             `<div class="days">
             <div class="numbers">${days}</div>days
         </div>
