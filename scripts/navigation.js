5653d849342632f547dcf018934caaab1f6b22c2
DavidREntwistle <davidrentwistle@gmail.com>
2024-05-27 21:49:52 +0100

    Styling changes

--- scripts/navigation.js
+++ scripts/navigation.js
@@ -1,18 +1,20 @@
 // navigation.js
 
 // Function to handle navigation link click
-function handleNavLinkClick(e) {
-    e.preventDefault();
-    
+// function handleNavLinkClick(e) {
+//     e.preventDefault();
+function handleNavLinkClick() {
+
     const path = this.getAttribute('href');
     const target = document.querySelector(path);
+    
     if (target) {
         // Scroll to the target section
-        target.scrollIntoView({
-            behavior: 'smooth',
-            block: 'start',
-            inline: 'start'
-        });
+        // target.scrollIntoView({
+        //     behavior: 'smooth',
+        //     block: 'start',
+        //     inline: 'start'
+        // });
 
         // Close the burger menu
         closeBurgerMenu();
@@ -33,11 +35,6 @@ function closeBurgerMenu() {
     document.getElementById("overlay").classList.remove('active');
     document.querySelector('.burger-menu').classList.remove('active');
 }
-
-// Add an event listener to all anchor links
-document.querySelectorAll('.nav-links a').forEach(anchor => {
-    anchor.addEventListener('click', handleNavLinkClick);
-});
 
 // Function to handle sticky navigation bar adjusted for SimpleBar
 function stickyNavBar() {
@@ -95,13 +92,13 @@ if (closeButton) {
     });
 }
 
-// Function to scroll to top of the page
-function scrollToTop() {
-    var simpleBarContainer = document.querySelector('#main-content .simplebar-content-wrapper');
-    if (simpleBarContainer) {
-        simpleBarContainer.scrollTo({ top: 0, behavior: 'smooth' });
-    }
-}
+// // Function to scroll to top of the page
+// function scrollToTop() {
+//     var simpleBarContainer = document.querySelector('#main-content .simplebar-content-wrapper');
+//     if (simpleBarContainer) {
+//         simpleBarContainer.scrollTo({ top: 0, behavior: 'smooth' });
+//     }
+// }
 
-// Add event listener for scroll to top button
-document.getElementById('scrollToTopBtn').addEventListener('click', scrollToTop);
+// // Add event listener for scroll to top button
+// document.getElementById('scrollToTopBtn').addEventListener('click', scrollToTop);
