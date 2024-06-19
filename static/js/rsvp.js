eec8b403ca6124d18252fd089c33965b05705e39
DavidREntwistle <davidrentwistle@gmail.com>
2024-06-19 13:59:56 +0100

    Validation fixes

--- static/js/rsvp.js
+++ static/js/rsvp.js
@@ -1,29 +1,4 @@
 // rsvp.js
-// Function to validate the form
-function validateForm() {
-    var firstName = document.getElementById("first-name").value.trim();
-    var lastName = document.getElementById("last-name").value.trim();
-    var attendYes = document.getElementById("attend-yes").checked;
-    var attendNo = document.getElementById("attend-no").checked;
-
-    if (firstName === "") {
-        alert("Please enter your first name");
-        return false;
-    }
-
-    if (lastName === "") {
-        alert("Please enter your last name");
-        return false;
-    }
-
-    if (!attendYes && !attendNo) {
-        alert("Please select an option");
-        return false;
-    }
-
-    return true; // Form is valid and can be submitted
-}
-
 function submitForm(event) {
     event.preventDefault(); // Prevent default form submission
 
