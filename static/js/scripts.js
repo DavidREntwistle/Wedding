556f4fe821a8b4d5382141102effaaf0e0b37e7e
DavidREntwistle <davidrentwistle@gmail.com>
2024-06-19 10:31:01 +0100

    Script improvement wip

--- static/js/scripts.js
+++ static/js/scripts.js
@@ -5,8 +5,10 @@ document.addEventListener("DOMContentLoaded", function() {
     // loadScript("scripts/password.js");
     loadScript("static/js/navigation.js");
     loadScript("static/js/countdown.js");
-    loadScript("static/js/table.js");
-    if (window.location.pathname === "/rsvp.html") {
+    if (window.location.pathname.endsWith("/accommodation") || window.location.pathname.endsWith("/accommodation.html")) {
+        loadScript("static/js/table.js");
+    }
+    if (window.location.pathname.endsWith("/rsvp") || window.location.pathname.endsWith("/rsvp.html")) {
         loadScript("static/js/rsvp.js");
     }
 
