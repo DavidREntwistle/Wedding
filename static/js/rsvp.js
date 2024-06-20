4e23ebd515111cc10da15fb0113ae9091a673920
DavidREntwistle <davidrentwistle@gmail.com>
2024-06-20 01:05:17 +0100

    Send single RSVP fix

--- static/js/rsvp.js
+++ static/js/rsvp.js
@@ -1,34 +1,60 @@
 // rsvp.js
-function submitForm(event) {
-    event.preventDefault(); // Prevent default form submission
+// This script handles form submission and redirects to success/error pages
+// Check if form has been submitted
+if (!window.formSubmitted) {
+    window.formSubmitted = false;
 
-    const form = document.getElementById('rsvp-form');
-    const formData = new FormData(form);
-
-    // Change submit button text to "Submitting..."
-    const submitButton = document.querySelector('button[type="submit"]');
-    submitButton.textContent = 'Submitting...';
-
-    fetch('https://script.google.com/macros/s/AKfycbxq3p_MdHOcKWq0pbJ_Z4M548gptL8h4CgFp2YyzbmcvyYeXuQ_XWasSv0M5pPeTvP8/exec', {
-        method: 'POST',
-        body: formData
-    })
-    .then(response => response.json())
-    .then(data => {
-        if (data.result === 'success') {
-            window.location.href = 'rsvp-submitted'; // Redirect to success page
-        } else {
-            window.location.href = 'rsvp-error'; // Redirect to error page
+    // Function to handle form submission
+    function submitForm(event) {
+        // Prevent multiple form submissions
+        if (window.formSubmitted) {
+            event.preventDefault();
+            return;
         }
-    })
-    .catch(error => {
-        console.error('Error:', error);
-        window.location.href = 'rsvp-error'; // Redirect to error page on fetch error
-    })
-    .finally(() => {
-        // Reset submit button text after submission is completed
-        submitButton.textContent = 'Submitted!';
-    });
+        window.formSubmitted = true;
+
+        console.log('Form submit button pressed');
+        event.preventDefault(); // Prevent default form submission
+
+        // Get form data
+        const form = document.getElementById('rsvp-form');
+        const formData = new FormData(form);
+
+        // Change submit button text to "Submitting..."
+        const submitButton = document.querySelector('button[type="submit"]');
+        submitButton.textContent = 'Submitting...';
+
+        console.log('Sending request...');
+        // Send form data to Google Apps Script
+        fetch('https://script.google.com/macros/s/AKfycbxq3p_MdHOcKWq0pbJ_Z4M548gptL8h4CgFp2YyzbmcvyYeXuQ_XWasSv0M5pPeTvP8/exec', {
+            method: 'POST',
+            body: formData
+        })
+        .then(response => {
+            console.log('Response received.');
+            return response.json();
+        })
+        .then(data => {
+            console.log('Data received:', data);
+            // Redirect to success/error page based on response
+            if (data.result === 'success') {
+                window.location.href = 'rsvp-submitted'; // Redirect to success page
+            } else {
+                window.location.href = 'rsvp-error'; // Redirect to error page
+            }
+        })
+        .catch(error => {
+            console.error('Error:', error);
+            window.location.href = 'rsvp-error'; // Redirect to error page on fetch error
+        })
+        .finally(() => {
+            // Reset submit button text after submission is completed
+            submitButton.textContent = 'Submitted!';
+            window.formSubmitted = false;
+        });
+    }
+
+    // Add event listener to form submission
+    document.getElementById('rsvp-form').addEventListener('submit', submitForm);
 }
 
-document.getElementById('rsvp-form').addEventListener('submit', submitForm);
--- static/js/scripts.js
+++ static/js/scripts.js
@@ -1,68 +1,34 @@
 // scripts.js
 // This script dynamically loads additional scripts and HTML
 
-const scriptSources = [
-    "static/js/navigation.js",
-    "static/js/countdown.js"
-];
-const htmlSources = [
-    ["static/utilities/navbar.html", "navbar-placeholder"]
-];
-
-document.addEventListener("DOMContentLoaded", () => {
-    const currentPage = document.location.pathname.split('/').slice(1).pop() || document.location.pathname;
-
-    if (currentPage === 'rsvp' || currentPage === 'rsvp.html') {
-        scriptSources.push("static/js/rsvp.js");
-    }
-    
-    if (currentPage === 'accommodation' || currentPage === 'accommodation.html') {
-        scriptSources.push("static/js/table.js");
-    }
-
-    // Load HTML first
-    htmlSources.forEach(([url, elementId]) => loadHTML(url, elementId));
-
-    // Load scripts sequentially
-    loadScriptsSequentially(scriptSources);
-});
-
-/**
- * Dynamically load a script file and return a promise.
- * @param {string} url - The URL to fetch the script from.
- * @returns {Promise} - A promise that resolves when the script is loaded.
- */
-function loadScript(url) {
-    return new Promise((resolve, reject) => {
-        const script = document.createElement("script");
-        script.src = url;
-        script.defer = true;
-        script.onload = () => resolve(`Loaded script: ${url}`);
-        script.onerror = () => reject(new Error(`Failed to load script: ${url}`));
-        document.body.appendChild(script);
-    });
-}
-
 /**
  * Load scripts sequentially.
  * @param {Array} scriptUrls - An array of script URLs to load.
+ * @returns {Promise} A promise that resolves when all scripts have loaded.
  */
 function loadScriptsSequentially(scriptUrls) {
-    scriptUrls.reduce((promise, scriptUrl) => {
-        return promise.then(() => loadScript(scriptUrl));
-    }, Promise.resolve())
-    .catch(error => {
-        console.error(error.message);
-    });
+    return scriptUrls.reduce((promise, scriptUrl) => {
+        return promise.then(() => {
+            return new Promise((resolve, reject) => {
+                const script = document.createElement("script");
+                script.src = scriptUrl;
+                script.defer = true;
+                script.onload = () => resolve();
+                script.onerror = () => reject(new Error(`Failed to load script: ${scriptUrl}`));
+                document.body.appendChild(script);
+            });
+        });
+    }, Promise.resolve());
 }
 
 /**
  * Load an HTML file into a specified element.
  * @param {string} url - The URL to fetch the HTML from.
  * @param {string} elementId - The ID of the element to load the HTML into.
+ * @returns {Promise} A promise that resolves when the HTML has been loaded.
  */
 function loadHTML(url, elementId) {
-    fetch(url)
+    return fetch(url)
         .then(response => {
             if (!response.ok) {
                 throw new Error(`Network response was not ok: ${response.statusText}`);
@@ -75,7 +41,7 @@ function loadHTML(url, elementId) {
                 element.innerHTML = data;
                 // Load scripts only after HTML is loaded to ensure placeholder elements are available
                 if (elementId === "navbar-placeholder") {
-                    loadScriptsSequentially(scriptSources);
+                    loadScriptsSequentially(["static/js/navigation.js", "static/js/countdown.js"]);
                 }
             } else {
                 console.error(`Element with ID ${elementId} not found`);
@@ -85,3 +51,33 @@ function loadHTML(url, elementId) {
             console.error(`There was a problem with the fetch operation for ${url}:`, error);
         });
 }
+
+document.addEventListener("DOMContentLoaded", () => {
+    const currentPage = document.location.pathname.split('/').slice(1).pop() || document.location.pathname;
+
+    const scriptSources = ["static/js/navigation.js", "static/js/countdown.js"];
+    const htmlSources = [["static/utilities/navbar.html", "navbar-placeholder"]];
+
+    if (currentPage === 'rsvp' || currentPage === 'rsvp.html') {
+        scriptSources.push("static/js/rsvp.js");
+    }
+    
+    if (currentPage === 'accommodation' || currentPage === 'accommodation.html') {
+        scriptSources.push("static/js/table.js");
+    }
+
+    // Load HTML first
+    htmlSources.forEach(([url, elementId]) => loadHTML(url, elementId));
+
+    // Check if we're on the rsvp-submitted or rsvp-error page
+    if (currentPage === 'rsvp-submitted' || currentPage === 'rsvp-error') {
+        // Ensure scripts related to form submission are not reloaded
+        const index = scriptSources.indexOf("static/js/rsvp.js");
+        if (index !== -1) {
+            scriptSources.splice(index, 1);
+        }
+    }
+
+    // Load scripts sequentially
+    loadScriptsSequentially(scriptSources);
+});
